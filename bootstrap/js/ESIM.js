508f2bf7a363032aae86b552cc8e8b2242d69528
YuChen <niyuchen.leaf@foxmail.com>
2018-05-09 12:04:13 +0800

    1.0版本修改

--- bootstrap/js/ESIM.js
+++ bootstrap/js/ESIM.js
@@ -30,7 +30,7 @@ function fnGetEid(){
             // 获取EID
             fnGetRetData('EID');
             // return EID_val;
-            console.log(EID_val);
+            // console.log(EID_val);
             showloading();
             var checkValue_ = fnCheckICCID();
             // console.log(typeof checkValue_);
@@ -208,30 +208,30 @@ function fnAjaxAPDU(A_id,A_data,A_valueid,A_num) {
         },
         success:function(msg){
             // document.getElementById(A_id).innerHTML = msg.responseText;
-            console.log(msg.responseText+ '------msg.responseText');
-            console.log(A_id + '---A_id----')
+            // console.log(msg.responseText+ '------msg.responseText');
+            // console.log(A_id + '---A_id----')
             if(A_id == 'APDU_a'){
                 APDU_a = msg.responseText;
-                console.log(APDU_a + '---APDU_a--1--')
+                // console.log(APDU_a + '---APDU_a--1--')
                 fnRunAPDUBACK('APDU_b',APDU_a,5);
-                console.log(APDU_b +'----卡片 发 a 回 b');
+                // console.log(APDU_b +'----卡片 发 a 回 b');
             }
             if(A_id == 'UPDATA_OK'){
                 UPDATA_OK = msg.responseText;
-                console.log(UPDATA_OK + '-------DP POST: data=04 + b(去掉前2字符) 回 UPDATA_OK');
+                // console.log(UPDATA_OK + '-------DP POST: data=04 + b(去掉前2字符) 回 UPDATA_OK');
                 // fnAjaxAPDU('APDU_c','05',APDU_b,2);
                 fnAjaxAPDU_c1_c4('APDU_c','05',APDU_b,2);
-                console.log(APDU_c+'------DP POST: data=05 + b(去掉前2字符) 回 APDU,c1,c2,c3,c4');
+                // console.log(APDU_c+'------DP POST: data=05 + b(去掉前2字符) 回 APDU,c1,c2,c3,c4');
                  
             }
         
             if(A_id == 'APDU_d'){
                  fnAjaxAPDU('DP_d','',APDU_c);
-                 console.log(DP_d);
+                 // console.log(DP_d);
             }
 
             if(A_id=='DP_d'){
-    console.log('----DP_d');
+    // console.log('----DP_d');
                                 // hiddenLoading();
                                        // 断开卡片
                                     // fnCardOff();
@@ -244,18 +244,18 @@ function fnAjaxAPDU(A_id,A_data,A_valueid,A_num) {
             // console.log(msg.response+ '------msg.response');
         },
         error:function(e){
-            console.log("error--"+ e);
-            console.log("error-e-"+ JSON.stringify(e));
+            // console.log("error--"+ e);
+            // console.log("error-e-"+ JSON.stringify(e));
         }
     })
 }
 
 function fnGetDP_iccid(){
     var data_val = new Date().getTime();
-    console.log("twtest_"+ data_val);
-    console.log(EID_val + '----EID_val');
-    console.log(document.getElementById("ChooseSIMCard").value+'---iccid');
-    console.log(typeof document.getElementById("ChooseSIMCard").value+'---document.getElementById("ChooseSIMCard").value');
+    // console.log("twtest_"+ data_val);
+    // console.log(EID_val + '----EID_val');
+    // console.log(document.getElementById("ChooseSIMCard").value+'---iccid');
+    // console.log(typeof document.getElementById("ChooseSIMCard").value+'---document.getElementById("ChooseSIMCard").value');
     ajax({
             type:"POST",
             url:"https://c9dp.roam2free.com:8443/roam2free-dp-service/gsma/rsp2/es2plus/downloadOrder",
@@ -271,8 +271,8 @@ function fnGetDP_iccid(){
                 //some js code
             },
             success:function(msg){
-                console.log(msg.responseText.iccid + '----msg');
-                console.log(msg.iccid + '----iccid');
+                // console.log(msg.responseText.iccid + '----msg');
+                // console.log(msg.iccid + '----iccid');
                 // if()
                 // document.getElementById(A_id).innerHTML = msg.responseText;
                 if(msg.status == 200){
@@ -294,23 +294,23 @@ function fnGetDP_iccid(){
                             // document.getElementById(A_id).innerHTML = msg.responseText;
                             if(msg.status == 200){
                                 fnAjaxAPDU('APDU_a','03',EID_val);
-                                console.log(APDU_a + '------APDU_000000a---data=03 + eid 返回 APDU,a');
+                                // console.log(APDU_a + '------APDU_000000a---data=03 + eid 返回 APDU,a');
                          
                             }
-                            console.log(msg.responseText+ '------msg.responseText');
+                            // console.log(msg.responseText+ '------msg.responseText');
                             // console.log(msg.response+ '------msg.response');
                         },
                         error:function(e){
-                            console.log("error" + "--" + e);
+                            // console.log("error" + "--" + e);
 
                         }
                     })
                 }
-                console.log(msg.responseText+ '------msg.responseText');
+                // console.log(msg.responseText+ '------msg.responseText');
                 // console.log(msg.response+ '------msg.response');
             },
             error:function(e){
-                console.log("error" + "--" + e);
+                // console.log("error" + "--" + e);
 
             }
         })
@@ -319,7 +319,7 @@ function fnGetDP_iccid(){
 
 function fnRunAPDUBACK(A_id,A_value,A_num) {
     // body...
-    console.log(A_value + '---back')
+    // console.log(A_value + '---back')
     var data_value = A_value;
     var A_valueback = '';
     if(!A_num){
@@ -328,14 +328,14 @@ function fnRunAPDUBACK(A_id,A_value,A_num) {
         A_valueback = data_value.substring(A_num);
     }   
     lReturn = myScc.RunAPDU(A_valueback);
-      console.log("CardOn----------- err:"+lReturn);
+      // console.log("CardOn----------- err:"+lReturn);
     if(lReturn!=0)
         alert("CardOn err:卡片 发 a 回 b"+"status:"+lReturn);
     // 获取命令返回状态字
     fnGetSW();
     // 获取返回数据
     fnGetRetData(A_id);
-    console.log(APDU_b + '------fnAjaxAPDU(UPDATA_OK,04,APDU_b,2);')
+    // console.log(APDU_b + '------fnAjaxAPDU(UPDATA_OK,04,APDU_b,2);')
     fnAjaxAPDU('UPDATA_OK','04',APDU_b,2);
     
 }
@@ -348,30 +348,32 @@ function fnRunAPDU_C(A_id,A_value) {
     // }
     var A_valueback =A_value;
     var result = A_valueback.split(",");
-    console.log(result[1] + '----1');
-    console.log(result[2] + '----2');
-    console.log(result[3] + '----3');
-    console.log(result[4] + '----4');
     // console.log(A_valueback + '------A_valueback')
     lReturn = myScc.RunAPDU(result[1]);
-    if(lReturn!=0)
+    if(lReturn!=0){
         alert("C1--:"+lReturn);
+        return;
+    }
     lReturn = myScc.RunAPDU(result[2]);
-    if(lReturn!=0)
+    if(lReturn!=0){
         alert("C2--:"+lReturn);
+        return;
+    }
     lReturn = myScc.RunAPDU(result[3]);
-    if(lReturn!=0)
+    if(lReturn!=0){
         alert("C3--:"+lReturn);
+        return;
+    }
     lReturn = myScc.RunAPDU(result[4]);
-    if(lReturn!=0)
+    if(lReturn!=0){
         alert("C4--:"+lReturn);
+        return;
+    }
     // 获取命令返回状态字
     fnGetSW();
     // 获取返回数据
-    console.log(A_id + '----a_id');
     fnGetRetData(A_id);
     sRes = myScc.GetRetData();
-    console.log(sRes + '----sres--')
 
         ajax({
             type:"POST",
@@ -386,15 +388,15 @@ function fnRunAPDU_C(A_id,A_value) {
             },
             success:function(msg){
                 // document.getElementById(A_id).innerHTML = msg.responseText;
-                console.log(msg.responseText+ '------msg.responseText');
+                // console.log(msg.responseText+ '------msg.responseText');
                 alert("ok")
                 hiddenLoading();
 
                 // console.log(msg.response+ '------msg.response');
             },
             error:function(e){
-                console.log("error--"+ e);
-                console.log("error-e-"+ JSON.stringify(e));
+                // console.log("error--"+ e);
+                // console.log("error-e-"+ JSON.stringify(e));
             }
         })
 
@@ -437,20 +439,17 @@ function fnGetICCID(){
             fnGetRetData('ICCID');
             if(ICCID_val.length >= 22){
                 var I_length = ICCID_val.length / 22 ;
-                console.log(I_length + '------')
-                 console.log(typeof I_length + '------')
                  var ICCID_list = []
                 for(var i = 0; i< I_length ;i++){
                       ICCID_list[i] = ChangeNums(ICCID_val.substring(i*22,(i+1)*22).substring(2));
                 }
-                console.log(ICCID_list + '-------')
-                // 断开卡片
+                        // 断开卡片
                         fnCardOff();
                         // 将读卡器释放
                         fnFreeReader();
-                alert(ICCID_list);
+                        alert(ICCID_list);
             }else{
-                // 断开卡片
+                        // 断开卡片
                         fnCardOff();
                         // 将读卡器释放
                         fnFreeReader();
@@ -487,7 +486,6 @@ function fnAjaxAPDU_c1_c4(A_id,A_data,A_valueid,A_num) {
         }
 
     }
-    console.log(A_value + '-----A_value')
     ajax({
         type:"POST",
         url:"https://c9dp.roam2free.com:8443/roam2free-dp-service/gsma/rsp2/es9plus/m2m/sendRes",
@@ -501,7 +499,7 @@ function fnAjaxAPDU_c1_c4(A_id,A_data,A_valueid,A_num) {
         },
         success:function(msg){
             // document.getElementById(A_id).innerHTML = msg.responseText;
-            console.log(msg.responseText+ '------msg.responseText');
+            // console.log(msg.responseText+ '------msg.responseText');
             APDU_c = msg.responseText;
             fnRunAPDU_C('APDU_d',APDU_c);
             // fnRunAPDU_C()
@@ -510,8 +508,8 @@ function fnAjaxAPDU_c1_c4(A_id,A_data,A_valueid,A_num) {
             // console.log(msg.response+ '------msg.response');
         },
         error:function(e){
-            console.log("error--"+ e);
-            console.log("error-e-"+ JSON.stringify(e));
+            // console.log("error--"+ e);
+            // console.log("error-e-"+ JSON.stringify(e));
         }
     })
 }
@@ -524,7 +522,7 @@ function fnListReaders(){
         s=new String(myScc.ListReaders());
          }
     catch(err){
-         console.log(err); // 可执行
+         // console.log(err); // 可执行
          alert("插件出错，请安装插件并允许ActiveX控件,使用IE浏览器打开");
          return;
     }
@@ -551,7 +549,6 @@ function fnSetReader(val){
         return false;
     }
     return true;
-    // console.log(lReturn + '-------lReturn ---------设置所使用的读卡器名称')
 }
 function fnConnect_Card(val) {
     // body...
@@ -563,7 +560,6 @@ function fnConnect_Card(val) {
     }
     lReturn = myScc.CardOn();
     if(lReturn!=0){
-        // alert("CardOn err:"+"连接智能卡.state:"+lReturn + ".");
         alert("连接智能卡出错.");
         return;
     }
@@ -575,7 +571,7 @@ function fnConnect_Card(val) {
 function fnCardOn(event) {
     lReturn = myScc.CardOn();
     if(lReturn!=0){
-        alert("CardOn err:"+"连接智能卡.state:"+lReturn + ".");
+        alert("连接智能卡出错.");
         return;
     }
 }
@@ -587,12 +583,10 @@ function fnGetATR() {
 //执行apdu命令
 function fnRunAPDU(val){
     lReturn = myScc.RunAPDU(val);
-    console.log(val + '-------fnRunAPDU ---------执行apdu命令')
     if(lReturn!=0){
         alert("CardOn err:"+"执行apdu命令"+val+".state:"+lReturn + ".");
         return;
     }
-    // console.log(lReturn + '-------lReturn ---------执行apdu命令')
 }
 //获取命令返回状态字
 function fnGetSW() {
@@ -600,25 +594,18 @@ function fnGetSW() {
     sSW = myScc.GetSW();
 
     if(sSW!=9000){
-        // console.log( sSW + '----1');
-        // alert("返回状态不是9000，Get Response失败！  sSW=" + sSW);
         if(sSW.substring(0,2)=="61" || sSW.substring(0,2)=="91"){
-            // console.log(sSW + '---2')
             fnRunAPDU("00C00000"+sSW.substring(2));
             fnGetSW();
         }
         if(sSW == "6A84"){
             alert("Cannot download profile to SIM card. There are already 10 profiles in SIM card. ");
         }
-        // console.log( sSW + '---3');
     }
-
-    console.log(sSW + '-------sSW ---------获取命令返回状态字')
 }
 //获取返回数据
 function fnGetRetData(val) {
     sRes = myScc.GetRetData();
-    // console.log(sRes + '-------sRes ---------获取返回数据')
     if(val == 'EID'){
         EID_val = sRes
         return EID_val;
@@ -633,12 +620,10 @@ function fnGetRetData(val) {
     }
     if(val == 'APDU_d'){
         APDU_d = sRes
-        // console.log(APDU_d + '------APDU_d----');
         return APDU_d;
     } 
     if(val == 'provisioning_id'){
         provisioning_id = sRes
-        console.log(provisioning_id + '------provisioning_id----');
         return provisioning_id;
     }  
     
@@ -647,24 +632,24 @@ function fnGetRetData(val) {
 //获取返回数据
 function fnGetRetData_value() {
     sRes = myScc.GetRetData();
-    // console.log(sRes + '-------sRes ---------获取返回数据')
     return sRes;
 }
 
 function fnCardOff(){
     lReturn = myScc.CardOff();
     if(lReturn!=0){
-        console.log("CardOff return "+lReturn);
+        // console.log("CardOff return "+lReturn);
         alert("断开卡片出错");
+        return;
     }
-    // console.log(lReturn + '-------lReturn ---------断开卡片')
 }
 function fnFreeReader(){
     lReturn = myScc.FreeReader();
 
     if(lReturn!=0){
-        console.log("FreeReader return "+lReturn);
+        // console.log("FreeReader return "+lReturn);
         alert("释放读卡器出错");
+        return
     }
     // console.log(lReturn + '-------lReturn ---------将读卡器释放');
 }
