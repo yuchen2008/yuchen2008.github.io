18c122c3cd3b5c38bfcfa3d07f8984b49fbd1b0d
YuChen <niyuchen.leaf@foxmail.com>
2018-05-15 11:20:56 +0800

    1

--- bootstrap/js/ESIM.js
+++ bootstrap/js/ESIM.js
@@ -251,12 +251,12 @@ function fnGetDP_iccid(){
             dataType:"json",
             data: JSON.stringify({
                 "orderId":"twtest_",
-                "eid":EID_val+"111",
-                "iccid":document.getElementById("ChooseSIMCard").value
+                "eid":EID_val+"111"
             }),
             beforeSend:function(){
                 //some js code
                 // "orderId":"twtest_"+ data_val,
+                 // "iccid":document.getElementById("ChooseSIMCard").value
             },
             success:function(msg){
                 // console.log(msg.responseText.iccid + '----msg');
